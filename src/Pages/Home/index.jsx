56a2fdfd41143fc03b382f4a5b894027b4f5c54b
corderoangel <corderoan777@gmail.com>
2024-12-07 20:13:14 -0500

    consumo api

--- src/Pages/Home/index.jsx
+++ src/Pages/Home/index.jsx
@@ -1,10 +1,28 @@
+import { useState } from "react";
 import Card from "../../components/Card";
 import Layout from "../../components/Layout";
+import { useEffect } from "react";
 
 function Home(){
+
+    const [items, setItems] = useState(null);
+
+    useEffect(() => {
+        fetch('https://api.escuelajs.co/api/v1/products')
+        .then(response => response.json())
+        .then(data => setItems(data))       
+    }, [])
+
     return(
         <Layout>
-            <Card/>
+            Home
+            <div className="grid gap-4 grid-cols-4 w-full max-w-screen-lg">
+            {
+                items?.map(item => (
+                    <Card key={item.id} data={item}/>
+                ))
+            }
+            </div>
         </Layout>
     );
 };
