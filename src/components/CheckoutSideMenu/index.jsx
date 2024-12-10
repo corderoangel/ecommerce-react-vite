a2ed03459c9081a57f2796fa5f33a0ee1d21fc04
corderoangel <corderoan777@gmail.com>
2024-12-10 09:03:43 -0500

    ordenes

--- src/components/CheckoutSideMenu/index.jsx
+++ src/components/CheckoutSideMenu/index.jsx
@@ -2,6 +2,7 @@ import { useContext } from 'react'
 import { XMarkIcon } from '@heroicons/react/24/solid'
 import { ShoppingCartContext } from '../../Context'
 import './styles.css'
+import OrderCard from '../OrderCard'
 
 const CheckoutSideMenu = () => {
   const context = useContext(ShoppingCartContext)
@@ -16,6 +17,18 @@ const CheckoutSideMenu = () => {
             className='h-6 w-6 text-black cursor-pointer'
             onClick={() => context.closeCheckoutSideMenu()}></XMarkIcon>
         </div>
+      </div>
+      <div className='px-6'>
+        {
+            context.cartProducts.map(product => (
+                <OrderCard 
+                key={product.id}
+                title={product.title}
+                imageUrl={product.images}
+                price={product.price}
+                />
+            ))
+        }
       </div>
     </aside>
   )
